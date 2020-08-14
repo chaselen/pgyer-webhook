03ac148121b6123b8cb744d532c60bc4eb02aa04
lancely <842184960@qq.com>
2020-08-14 16:39:10 +0800

    修改命名

--- app/controller/appWebhook.js
+++ app/controller/appWebhook.js
@@ -5,7 +5,7 @@ const Controller = require('egg').Controller;
 /**
  * App的Webhook控制器
  */
-class AppWebHookController extends Controller {
+class AppWebhookController extends Controller {
   /**
    * 版本更新
    * {"action":"应用更新","title":"QQ","link":"https://www.pgyer.com/abcd","message":"您的应用QQ有了新的版本(0.4.0)更新。","type":"updateVersion","os_version":"0.4.0","build_version":"2","created":"2020-07-15 10:50:54","updated":"2020-07-15 10:50:54","timestamp":1594781454,"appsize":"26662364","device_type":"iOS","notes":""}
@@ -29,4 +29,4 @@ class AppWebHookController extends Controller {
   }
 }
 
-module.exports = AppWebHookController;
+module.exports = AppWebhookController;
